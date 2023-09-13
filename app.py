408a7dcfe8b0f84140c425c1fb98aa6c006ee6c5
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-09-13 16:10:09 +0300

    Second version of Bowyer-Watson ready
    This one works much better. Sometimes nodes may spawn in a straight line and in those cases the algorithm doesn work. I'll try to get rid of this problem later

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-#right now I'm trying to create a successful triangulation with bowyer-watson algorithm with my program
-#at the moment the program plots a single triangle and a circle around it
+'''
+At the moment this program visualises Delaunay triangulation with Bowyer-Watson algorithm.
+It has problems with input size 3 but other inputs seem to work.
+'''
+
 import itertools
 import pygame
 from random import *
@@ -34,15 +37,16 @@ class Triangle():
         self.circumcenter = findCircumcenter([self.node_a, self.node_b, self.node_c])
         self.circum_circles_radius = distanceBetweenPoints(self.circumcenter, self.node_a)
 
-        self.edges = [[self.node_a, self.node_b],
+        self.nodes = [self.node_a, self.node_b, self.node_c]
+        self.triangle_edges = [[self.node_a, self.node_b],
                       [self.node_b, self.node_c],
                       [self.node_c, self.node_a]]
     
     def __str__(self):
-        return f"Triangle number {self.id}"
+        return f'Triangle with edges {self.triangle_edges}'
     
     def __repr__(self):
-        return f'Triangle with edges {self.edges}'
+        return f'Triangle with edges {self.triangle_edges}'
 
     def __eq__(self, other): 
         if not isinstance(other, Triangle):
@@ -54,8 +58,11 @@ class Triangle():
     def showID(self):
         return self.id
 
-    def edges(self):
-        return self.edges
+    def plotEdges(self):
+        return self.triangle_edges
+    
+    def showNodes(self):
+        return self.nodes
 
     def circumCenter(self):
         return self.circumcenter
@@ -64,7 +71,7 @@ class Triangle():
         return self.circum_circles_radius
     
     def plot(self):
-        for e in self.edges:
+        for e in self.triangle_edges:
             pygame.draw.line(display, GREEN, e[0], e[1])
 
     def circumCircle(self):
@@ -96,10 +103,6 @@ def plotCenterPoints(coordinates: list):
             center_point_y = (coordinates[i][1]+coordinates[next][1])/2
 
             pygame.draw.circle(display, RED, (center_point_x, center_point_y), 3)
-
-#coordinates = [((randint(100, displayWidth-100), (randint(100, displayHeight-100)))) for i in range(counter)]
-#coordinates = [(178, 169), (585, 179), (513, 393)]
-#print(coordinates)
 
 #find cartesian coordinates of the circumcenter of a triangle
 def findCircumcenter(coordinates: list):
@@ -144,27 +147,20 @@ def generateCoordinates(count: int):
 def areEdgesEqual(edge_1: list, edge_2: list):
     return edge_1[0] == edge_2[0] and edge_1[1] == edge_2[1] or edge_1[0] == edge_2[1] and edge_1[1] == edge_2[0]
 
+#generate Delaunay triangulation
 def bowyerWatson(nodelist: list):
     triangulation = []
 
-    #algorithm part1
     super_triangle = Triangle(super_coordinates[0], super_coordinates[1], super_coordinates[2])
     super_triangle_nodes = []
 
-    super_triangle_edges = super_triangle.edges
+    super_triangle_edges = super_triangle.plotEdges()
+    print(super_triangle_edges)
     for edge in super_triangle_edges:
         super_triangle_nodes.append(edge[0])
     print(super_triangle_nodes)
-    #tri2 = Triangle((1, 4), (10, 10), (100, 100))
-    #tri3 = Triangle((1, 4), (10, 10), (100, 100))
-    #print(tri2.edges)
-    #print(areEdgesEqual(tri2.edges[1], tri2.edges[1]))
 
     triangulation.append(super_triangle)
-    #triangulation.append(tri2)
-    #triangulation.append(tri3)
-    #print(triangulation)
-    #print(triangulation[1] == tri2)
 
     for node in nodelist:
 
@@ -174,65 +170,60 @@ def bowyerWatson(nodelist: list):
             circumcenter = triangle.circumCenter()
             circum_circles_radius = triangle.ccRadius()
 
-            #algorithm part 2: distance between the triangles circumcenter and the node
             distance = distanceBetweenPoints(node, circumcenter)
             if distance < circum_circles_radius:
                 badTriangles.append(triangle)
-                #print(f'distance between {circumcenter} and {node} is {distance} which is less than {circum_circles_radius}')
-            
+                        
         polygon = []
-        #algorithm part 3: define the polygonal hole
+
         for triangle in badTriangles:
-            print(f'part 3 for {triangle}')
-            edges = triangle.edges
+            edges = triangle.plotEdges()
             for edge in edges:
-                print(f'looking at edge {edge}')
                 found = False
-                
                 for otherTriangle in badTriangles:
-                    print(f'comparing edge {edge} to {otherTriangle}')
-                    other_triangles_edges = otherTriangle.edges
+                    other_triangles_edges = otherTriangle.plotEdges()
                     if otherTriangle == triangle:
                         continue
 
                     for other_edge in other_triangles_edges:
                         if areEdgesEqual(other_edge, edge):
-                            print(f'similar edges {edge} and {other_edge}')
                             found = True
                 if not found:
-                    print(f'adding edge {edge} to polygon')
                     polygon.append(edge)
-        print(f'polygon: {polygon}')
 
+        delete_triangles = []
         for triangle in triangulation:
             if triangle in badTriangles:
-                #print(triangulation)
-                print('this triangle is bad')
-                triangulation.remove(triangle)
-                print(f'triangulation after deletion: {triangulation}')
+                delete_triangles.append(triangle)
+        
+        for triangle in delete_triangles:
+            triangulation.remove(triangle)
         
         for edge in polygon:
             newTriangle = Triangle(edge[0], edge[1], node)
-            print(f'new triangle is {newTriangle}')
             triangulation.append(newTriangle)
+        
+        #for triangle in triangulation:
+        #    triangle.plot()
+        #    pygame.display.flip()
     
+    remove_triangles = []
+
     for triangle in triangulation:
-        edges = triangle.edges
+        nodes = triangle.showNodes()
         found = False
-        for edge in edges:
+        for n in nodes:
             if found:
                 continue
-            if edge[0] in super_triangle_nodes or edge[1] in super_triangle_nodes:
-                triangulation.remove(triangle)
+            if n in super_triangle_nodes:
                 found = True
-                
+                remove_triangles.append(triangle)
+    for triangle in remove_triangles:
+        triangulation.remove(triangle)
+   
     return triangulation
 
-
-
-bowyerWatson(generateCoordinates(3))
-
-'''
+#visualising with pygame
 while True:
     for tapahtuma in pygame.event.get():
         if tapahtuma.type == pygame.QUIT:
@@ -243,23 +234,20 @@ while True:
     coordinates = generateCoordinates(counter)
 
     for c in coordinates:
-        pygame.draw.circle(display, BLUE, c, 2)
+        pygame.draw.circle(display, BLUE, c, 4)
 
     t = Triangle(super_coordinates[0], super_coordinates[1], super_coordinates[2])
     t.plot()
-    t.circumCircle()
 
-    #area where points can be plotted
-    pygame.draw.line(display, RED, (x_min, y_min), (x_max, y_min))
-    pygame.draw.line(display, RED, (x_min, y_min), (x_min, y_max))
-    pygame.draw.line(display, RED, (x_min, y_max), (x_max, y_max))
-    pygame.draw.line(display, RED, (x_max, y_min), (x_max, y_max))
+    triangulation = bowyerWatson(coordinates)
 
-    #pygame.draw.circle(display, BLUE, displayCenter, 2)
+    for triangle in triangulation:
+        triangle.plot()
 
-    #plotATriangle(coordinates)
-    #plotCircumcircle(coordinates)
+    #pygame.draw.line(display, RED, (x_min, y_min), (x_max, y_min))
+    #pygame.draw.line(display, RED, (x_min, y_min), (x_min, y_max))
+    #pygame.draw.line(display, RED, (x_min, y_max), (x_max, y_max))
+    #pygame.draw.line(display, RED, (x_max, y_min), (x_max, y_max))
 
     pygame.display.flip()
     pygame.time.wait(500)
-'''
